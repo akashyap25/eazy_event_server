974abd47d1147a2e54dabbb7eeb223b50dc15567
akashyap25 <anuragkashyap026@gmail.com>
2024-07-29 17:38:27 +0530

    cors manual middleware

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const eventRoutes = require('./routes/eventRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -16,21 +14,23 @@ const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL;
 const port = process.env.PORT || 5000;
 const app = express();
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || origin.startsWith(CLIENT_BASE_URL)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
-
-app.use(cors(corsOptions));
 app.use(bodyParser.json()); // Ensure this is used before your routes
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && origin.startsWith(CLIENT_BASE_URL)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  }
+  next();
+});
 
 app.use('/api/events', eventRoutes);
 app.use('/api/categories', categoryRoutes);
